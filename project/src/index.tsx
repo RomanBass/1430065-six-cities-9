e99f845a91519e31230d3f85e7a5171761881342
Roman Katrovsky <katrovsky@gmail.com>
2022-02-20 14:01:15 +0300

    проблема с tabindex

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import Main from './components/main/main';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>,
+  document.getElementById('root'));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
   </React.StrictMode>,
   document.getElementById('root'));
