a25404d6387ab880d2594d6c4333cf60d3de6a82
Roman Katrovsky <katrovsky@gmail.com>
2022-02-26 15:04:30 +0300

    Реализует отрисовку App в src/index.tsx

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import Main from './components/main/main';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'));
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Main />
   </React.StrictMode>,
   document.getElementById('root'));
