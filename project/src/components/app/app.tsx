f2905a846b75fdaef4cc740862ed06c6912fc3f0
Roman Katrovsky <katrovsky@gmail.com>
2022-03-15 17:24:15 +0300

    реализует проверку авторизации при переходе на страницу favorites

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,6 +4,7 @@ import Login from '../login/login';
 import Favorites from '../favorites/favorites';
 import Room from '../property/property';
 import NotFound from '../not-found/not-found';
+import PrivateRoute from '../private-route';
 
 type AppScreenProps = {
   rentalOffersNumber: number;
@@ -15,7 +16,12 @@ function App({rentalOffersNumber}: AppScreenProps): JSX.Element {
       <Routes>
         <Route index element={<Main rentalOffersNumber={rentalOffersNumber} />} />
         <Route path='login' element={<Login />} />
-        <Route path='favorites' element={<Favorites />} />
+        <Route path='favorites' element={
+          <PrivateRoute>
+            <Favorites />
+          </PrivateRoute>
+        }
+        />
         <Route path='offer/:id' element={<Room />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
